892fbf117f688847d63cbfb94a6af5ed3b44bbb9
chad <chad@10.18.151.91>
2015-10-08 09:46:45 -0600

    10/8/2015 team assignment

---
+++ hogwartsSchool/src/byui/cit260/hogwartsschool/model/GeneralScenes.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.hogwartsSchool.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author chad
+ */
+public class GeneralScenes implements Serializable{
+    private String description;
+
+    public GeneralScenes() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+      
+    public String toString() {
+        return "GeneralScenes{" + "description=" + description + '}';
+    }
+
+   
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.description);
+        return hash;
+    }
+
+    
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final GeneralScenes other = (GeneralScenes) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
